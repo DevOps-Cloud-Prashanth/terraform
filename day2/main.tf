bf270556552b03fe0e59ce6e12e63af3715d529f
DevOps-Cloud-Prashanth <prashanthganapuram067@gmail.com>
2025-10-28 13:11:17 +0530

    add

---
+++ day2/main.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name= "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "name" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.0.0/24"
+    tags = {
+      Name= "mysubnet"
+    }
+  
+}
+
+resource "aws_instance" "name" {
+    ami = var.ami
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.name.id
+    tags = {
+      Name= "terraform-instance"
+    }
+}
---
+++ day2/variable.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+  description = "ami-id"
+  default = ""
+  type = string
+}
+
+variable "instance_type" {
+    description = "instance_type"
+    default = ""
+    type = string
+  
+}
