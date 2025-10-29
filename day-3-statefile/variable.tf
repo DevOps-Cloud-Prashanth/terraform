57ec0fc0bf5e6bd2c9bbad8dafab74fa38812965
DevOps-Cloud-Prashanth <prashanthganapuram067@gmail.com>
2025-10-29 16:51:51 +0530

    day-3

---
+++ day-3-statefile/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+    description = "ami created"
+    default = ""
+    type = string
+}
+
+variable "instance_type" {
+    description = "instance created"
+    default = ""
+    type = string
+  
+}
