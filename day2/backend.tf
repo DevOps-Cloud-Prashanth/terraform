c025b0ccf56e71cb802355f6d5e8ae62c53d2cb8
DevOps-Cloud-Prashanth <prashanthganapuram067@gmail.com>
2025-10-31 14:09:48 +0530

    Add day1 basic code

--- day2/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "ihihiibucket1aws"
-    key    = "day-2/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
--- day3 statefile/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "name" {
-  instance_type = var.instance_type
-  ami = var.ami_id
-    tags = {
-      Name= "terraform-instance"
-    }
-}
-  
