8316972fbbb2b46ca76199a2033368cfc4a4fd6a
DevOps-Cloud-Prashanth <prashanthganapuram067@gmail.com>
2025-11-01 13:52:50 +0530

    day6

--- day6import/main.tf
+++ day6import/main.tf
@@ -13,3 +13,20 @@ resource "aws_instance" "name" {
 #terraform import aws_instance.name i-010ab9de2cb0cbe71 #this command is used to import instance resources from aws console through terraform
 
 #terraform import aws_s3_bucket prashanth12376576 #this command is used to import s3 resources from aws console through terraform
+
+
+
+
+
+#note----if we want any specific resource to import we have to do main.tf is empty resource #
+
+       #example below
+
+#resource "aws_instance" "name" {
+  
+#}
+
+
+# note----after completion of import we have add details of that resource in main.tf
+
+#we have to match the terraform.tfstate file with main.tf
